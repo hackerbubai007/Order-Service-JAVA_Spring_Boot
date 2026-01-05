3657d3d40330dffb1da0122407efa0097f00163f
Dipankar Mandal <71397445+hackerbubai007@users.noreply.github.com>
2026-01-05 11:50:38 +0530

    Update OrderInfo.java

--- spring-REST-API-sample/src/main/java/com/zepto/order/bean/OrderInfo.java
+++ spring-REST-API-sample/src/main/java/com/zepto/order/bean/OrderInfo.java
@@ -10,36 +10,6 @@ public class OrderInfo {
 
 	private String qty;
 
-	public String getItemName() {
-		return itemName;
-	}
-
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
-
-	public String getPrice() {
-		return price;
-	}
-
-	public void setPrice(String price) {
-		this.price = price;
-	}
-
-	public String getProductId() {
-		return productId;
-	}
-
-	public void setProductId(String productId) {
-		this.productId = productId;
-	}
-
-	public String getQty() {
-		return qty;
-	}
-
-	public void setQty(String qty) {
-		this.qty = qty;
-	}
+	
 
 }
